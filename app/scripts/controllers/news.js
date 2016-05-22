d2455abbf0ae197e3b9bebb8a083b5395bc4276e
Gastón Ambrogi <gaston.ambrogi@gmail.com>
2016-05-21 22:59:02 -0300

    Fixed active screen news animation.

--- app/scripts/controllers/news.js
+++ app/scripts/controllers/news.js
@@ -8,28 +8,39 @@
  * Controller of the wavesApp
  */
 angular.module('wavesApp')
-  .controller('NewsCtrl', function ($scope, $interval) {
-    $scope.currentNotice = 0;
-    $scope.latestNews = [
-      'Noticia1Noticia1Noticia1Noticia1Noticia1Noticia1Noticia1Noticia1Noticia1',
-      'Noticia2Noticia2Noticia2Noticia2Noticia2Noticia2Noticia2Noticia2Noticia2',
-      'Noticia3Noticia3Noticia3Noticia3Noticia3Noticia3Noticia3Noticia3Noticia3',
-      'Noticia4Noticia4Noticia4Noticia4Noticia4Noticia4Noticia4Noticia4Noticia4',
-      'Noticia5Noticia5Noticia5Noticia5Noticia5Noticia5Noticia5Noticia5Noticia5',
-      'Noticia6Noticia6Noticia6Noticia6Noticia6Noticia6Noticia6Noticia6Noticia6',
-      'Noticia7Noticia7Noticia7Noticia7Noticia7Noticia7Noticia7Noticia7Noticia7',
-      'Noticia8Noticia8Noticia8Noticia8Noticia8Noticia8Noticia8Noticia8Noticia8',
-      'Noticia9Noticia9Noticia9Noticia9Noticia9Noticia9Noticia9Noticia9Noticia9',
-      'Noticia10Noticia10Noticia10Noticia10Noticia10Noticia10Noticia10Noticia10',
-    ];
+  .controller('NewsCtrl', function ($scope, $interval, $timeout) {
+    $scope.pageSize = 10;
 
-    $scope.currentNews = $scope.latestNews[$scope.currentNotice];
+    $scope.news={
+      latest: [
+        'Noticia1Noticia1Noticia1Noticia1Noticia1Noticia1Noticia1Noticia1Noticia1',
+        'Noticia2Noticia2Noticia2Noticia2Noticia2Noticia2Noticia2Noticia2Noticia2',
+        'Noticia3Noticia3Noticia3Noticia3Noticia3Noticia3Noticia3Noticia3Noticia3',
+        'Noticia4Noticia4Noticia4Noticia4Noticia4Noticia4Noticia4Noticia4Noticia4',
+        'Noticia5Noticia5Noticia5Noticia5Noticia5Noticia5Noticia5Noticia5Noticia5',
+        'Noticia6Noticia6Noticia6Noticia6Noticia6Noticia6Noticia6Noticia6Noticia6',
+        'Noticia7Noticia7Noticia7Noticia7Noticia7Noticia7Noticia7Noticia7Noticia7',
+        'Noticia8Noticia8Noticia8Noticia8Noticia8Noticia8Noticia8Noticia8Noticia8',
+        'Noticia9Noticia9Noticia9Noticia9Noticia9Noticia9Noticia9Noticia9Noticia9',
+        'Noticia10Noticia10Noticia10Noticia10Noticia10Noticia10Noticia10Noticia10',
+      ], 
+      current: "",
+      currentIndex: 0,
+      show: true
+    };
+
 
     $interval(function(){
-      $scope.currentNotice = ($scope.currentNotice + 1) % $scope.latestNews.length;
-      $scope.currentNews = $scope.latestNews[$scope.currentNotice];
-      if($scope.currentNotice === 0){
+      $scope.news.show = false;
+      $scope.news.currentIndex = ($scope.news.currentIndex + 1) % $scope.news.latest.length;
+      
+      $timeout(function () {
+        $scope.news.current = $scope.news.latest[$scope.news.currentIndex];
+        $scope.news.show = true;
+      }, 500);
+
+      if($scope.news.currentIndex === 0){
         console.log('WAVES::' + 'Request new news page');
       }
-    }, 5000);
+    }, 10000);
   });

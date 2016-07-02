01f71a565035a1ed381e9f31bad830208c667640
Gastón Ambrogi <gaston.ambrogi@gmail.com>
2016-07-02 16:02:33 -0300

    Added Weather response view.

--- app/scripts/controllers/weather.js
+++ app/scripts/controllers/weather.js
@@ -9,13 +9,13 @@
  */
 angular.module('wavesApp')
   .controller('WeatherCtrl', ['$scope', function ($scope) {
-      $scope.weatherToday = {day: new Date(new Date().getTime()+(0*24*60*60*1000)), min:"15.0", max: "19.0", icon: "sunny"};
+      $scope.weatherToday = {day: new Date(new Date().getTime()+(0*24*60*60*1000)), min:"15.0", max: "19.0", icon: "day-sunny"};
   
       $scope.forecast = [
-        {day: new Date(new Date().getTime()+(0*24*60*60*1000)), min:"15.0", max: "19.0", icon: "sunny"},
-        {day: new Date(new Date().getTime()+(1*24*60*60*1000)), min:"15.0", max: "19.0", icon: "sunny"},
-        {day: new Date(new Date().getTime()+(2*24*60*60*1000)), min:"15.0", max: "19.0", icon: "sunny"},
-        {day: new Date(new Date().getTime()+(3*24*60*60*1000)), min:"15.0", max: "19.0", icon: "sunny"},
-        {day: new Date(new Date().getTime()+(4*24*60*60*1000)), min:"15.0", max: "19.0", icon: "sunny"},
+        {day: new Date(new Date().getTime()+(1*24*60*60*1000)), min:"15.0", max: "19.0", icon: "day-sunny"},
+        {day: new Date(new Date().getTime()+(2*24*60*60*1000)), min:"15.0", max: "19.0", icon: "day-sunny"},
+        {day: new Date(new Date().getTime()+(3*24*60*60*1000)), min:"15.0", max: "19.0", icon: "day-sunny"},
+        {day: new Date(new Date().getTime()+(4*24*60*60*1000)), min:"15.0", max: "19.0", icon: "day-sunny"},
+        {day: new Date(new Date().getTime()+(5*24*60*60*1000)), min:"15.0", max: "19.0", icon: "day-sunny"},
       ];
     }]);
---
+++ app/scripts/directives/responses/weatherresponse.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name wavesApp.directive:weatherResponse
+ * @description
+ * # weatherResponse
+ */
+angular.module('wavesApp')
+  .directive('weatherResponse', function () {
+    return {
+      templateUrl: '/views/directives/responses/weather.html',
+      replace: true,
+      restrict: 'E',
+      controller: 'WeatherCtrl'
+    };
+  });

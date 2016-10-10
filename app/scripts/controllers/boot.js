dde226440f3831bd89b91233b67455d0de885900
Gastón Ambrogi <gaston.ambrogi@gmail.com>
2016-10-10 16:27:13 -0300

    Bad condition evaluation.

--- app/scripts/controllers/boot.js
+++ app/scripts/controllers/boot.js
@@ -20,7 +20,7 @@ angular.module('wavesApp')
       InflectionsAPIService.fetchStartupData([hollidaysWord, weatherWord, newsWord])
         .then(function(results){
           results.forEach(function(result){
-            if(result.data.content.body===undefined){
+            if(result.data.content.body!==undefined){
               localStorageService.set(result.data.attributes.query_words.join(', '), result.data.content.body);
             }else{
               console.error("Check on Inflections why the service with query_word \""+result.data.attributes.query_words.join(', ')+"\" returns undefined.");
